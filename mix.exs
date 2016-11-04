753627efd339538d62d13cee4226c23f8b02dacc
Ashley Ellis <anellis12@gmail.com>
2016-11-04 15:42:31 -0400

    Use Elixir 1.3
    * This keeps us current and fixes CI by giving us access to the
    ExUnit `describe` function

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Brady.Mixfile do
   def project do
     [app: :brady,
      version: "0.0.4",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      description: "Template helpers for Phoenix applications",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
