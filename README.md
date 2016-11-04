79413e9b4a844efdc850f7a0880294281ec007e3
Ashley Ellis <anellis12@gmail.com>
2016-11-04 16:18:14 -0400

    Release version 0.0.5

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Add brady to your list of dependencies in `mix.exs`:
 ```elixir
   def deps do
     [
-      {:brady, "~> 0.0.4"},
+      {:brady, "~> 0.0.5"},
     ]
   end
 ```
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Brady.Mixfile do
 
   def project do
     [app: :brady,
-     version: "0.0.4",
+     version: "0.0.5",
      elixir: "~> 1.3",
      description: "Template helpers for Phoenix applications",
      build_embedded: Mix.env == :prod,
