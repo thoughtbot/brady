ac164a46366366844398bbdc877c01f24aed301e
Jason Draper <jason@drapergeek.com>
2017-03-02 13:24:59 -0500

    Release version 0.0.6

--- doc/dist/sidebar_items.js
+++ doc/dist/sidebar_items.js
@@ -1,1 +1,1 @@
-sidebarNodes={"exceptions":[],"extras":[{"id":"api-reference","title":"API Reference","headers":[]}],"modules":[{"id":"Brady","title":"Brady","functions":[{"id":"body_class/1","anchor":"body_class/1"}]}],"protocols":[]}
+sidebarNodes={"exceptions":[],"extras":[{"id":"api-reference","title":"API Reference","headers":[]}],"modules":[{"id":"Brady","title":"Brady","functions":[{"id":"body_class/1","anchor":"body_class/1"},{"id":"inline_svg/2","anchor":"inline_svg/2"}]}],"protocols":[]}
