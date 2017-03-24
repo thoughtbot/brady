750369d527aef78a0571fb34db6f5b595733d4ca
Jason Draper <jason@drapergeek.com>
2017-03-24 15:01:49 -0400

    Clean up the mix file

--- mix.exs
+++ mix.exs
@@ -2,33 +2,22 @@ defmodule Brady.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :brady,
-     version: "0.0.6",
-     elixir: "~> 1.3",
-     description: "Template helpers for Phoenix applications",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     package: package()
-   ]
+    [
+      app: :brady,
+      build_embedded: Mix.env == :prod,
+      deps: deps(),
+      description: "Template helpers for Phoenix applications",
+      elixir: "~> 1.3",
+      package: package(),
+      start_permanent: Mix.env == :prod,
+      version: "0.0.6",
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:earmark, "~>0.1", only: :dev},
@@ -40,9 +29,10 @@ defmodule Brady.Mixfile do
 
   defp package do
     [
-    files: ["lib", "mix.exs", "README*", "LICENSE*"],
-    maintainers: ["Ashley Ellis"],
-    licenses: ["MIT"],
-    links: %{"Github" => "https://github.com/thoughtbot/brady"}]
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/thoughtbot/brady"},
+      maintainers: ["Ashley Ellis", "Jason Draper"],
+    ]
   end
 end
